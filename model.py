28dee97f122dda4af874f395a4743aa082f533d1
ZacharyChenpk <954617373@qq.com>
2019-08-01 21:47:37 +0800

    start working

--- model.py
+++ model.py
@@ -1,6 +1,70 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from locked_dropout import LockedDropout
 from ON_LSTM import ONLSTMStack
 
+class Pos_choser(nn.Module):
+	### take in the tree currently generated, and return the distribution of positions to insert the next node
+	def __init__(self, ntoken, hidden_dim, node_dim, tree_dim, dropout=0.1)
+		super(Pos_choser,self).__init__()
+		self.drop = nn.Dropout(dropout)
+	###
+		self.gcn = GCN(tree_dim)
+	###
+		self.inp_dim = hidden_dim + node_dim + tree_dim
+		self.hidden_dim = hidden_dim
+		self.node_dim = node_dim
+		self.tree_dim = tree_dim
+		self.score_cal = nn.Sequential(nn.Linear(self.inp_dim, self.hidden_dim), 
+			nn.ReLU(),
+			self.drop,
+			nn.Linear(self.hidden_dim, 1))
+
+	def forward(self, cur_tree)
+		num_samples = cur_tree.size(0)
+		cur_tree.make_index(0)
+	###
+		self.gcn(cur_tree)
+		node_hidden = cur_tree.hidden_states ### should be a 2-D tensor
+	###
+		leaves = cur_tree.leaves(True)
+		leave_inds = [x.index for x in leaves]
+		leave_states = node_hidden[leave_inds]
+		scores = map(self.score_cal.forward, leave_states)
+		scores = F.softmax(scores)
+		return leaves, scores
+
+	def init_hidden(self):
+		return self.score_cal.init_hidden()
+
+class sentence_encoder(nn.Module):
+	def __init__(self, ntoken, h_dim, emb_dim, nlayers, chunk_size, wdrop=0, dropouth=0.5):
+		super(sentence_encoder, self).__init__()
+		self.lockdrop = LockedDropout()
+		self.hdrop = nn.Dropout(dropouth)
+		self.encoder = nn.Embedding(ntoken, emb_dim)
+		self.rnn = ONLSTMStack(
+			[emb_dim] + [h_dim] + nlayers,
+			chunk_size = chunk_size,
+			dropconnect = wdrop,
+			dropout = dropouth
+		)
+		initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+        self.h_dim = h_dim
+        self.emb_dim = emb_dim
+        self.nlayers = nlayers
+        self.ntoken = ntoken
+        self.chunk_size = chunk_size
+        self.wdrop = wdrop
+        self.dropouth = dropouth
+
+    def forward(self, inp_sentence, hidden)
+
+
+class word_choser(nn.Module):
+	def __init__(self, ntoken, hidden_dim):
--- tree.py
+++ tree.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 class Tree():
 	def __init__(self, root):
 		self.root = root
+		self.index = 0
 		self.left = False
 		self.right = False
 	def horizontal_scan(self):
@@ -19,10 +20,35 @@ class Tree():
 		if (not self.left) and (not self.right):
 			return [self]
 		tmp = []
-		if contain_single and not (self.left and self.right)
+		if contain_single and not (self.left and self.right):
 			tmp = [self]
 		if self.left:
 			tmp = self.left.leaves() + tmp
 		if self.right:
 			tmp = tmp + self.right.leaves()
-	
+		return tmp
+	def nodenum(self):
+		tmp = 1
+		if self.left:
+			tmp = tmp + self.left.nodenum()
+		if self.right:
+			tmp = tmp + self.right.nodenum()
+		return tmp
+	def tree2graph(self):
+		### just act like its name
+		return
+	def make_index(self, start_i = 0):
+		### attach horizontial index to the root node of its subtree
+		if self.left == False and self.right == False:
+			self.index = start_i
+			return start_i
+		nodes = start_i
+		if self.left:
+			nodes = self.left.make_index(start_i) + 1
+		self.index = nodes
+		if self.right:
+			return self.right.make_index(nodes + 1)
+		return nodes
+
+
+
