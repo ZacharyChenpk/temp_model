7e753ff42a9acf089c2e36fae41227ace686c72b
tsy19025 <45425656+tsy19025@users.noreply.github.com>
2019-10-10 10:07:22 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -11,10 +11,10 @@ import torch.nn as nn
 
 class ModelEncoder(nn.Module):
     def __init__(self, embedding_size, hidden_state_size, layer_size, sentence_embedding_size,
-                 chunk_size, gcn_input_size, gcn_hidden_size, gcn_output_size, dropout = 0.5):
+                 chunk_size, gcn_hidden_size, gcn_output_size, dropout = 0.5):
         super(ModelEncoder, self).__init__()
         
-        self.gcn_input_size = gcn_input_size
+        self.gcn_input_size = embedding_size
         self.gcn_hidden_size = gcn_hidden_size
         self.gcn_output_size = gcn_output_size
         self.dropout = dropout
@@ -22,6 +22,7 @@ class ModelEncoder(nn.Module):
         self.encoder = ONLSTMStack([embedding_size] + [hidden_state_size] * (layer_size - 1) + [sentence_embedding_size], chunk_size)
     
     def forward(self, sentences, hidden):
+        # sentences size: length * batch_size * word_embedding_size
         raw_output, hidden_cell, raw_outputs, outputs, distance = self.encoder(sentences, hidden)
         # hidden layer_size * batch_size * hidden_state_size
         
@@ -31,10 +32,11 @@ class ModelEncoder(nn.Module):
         distances = distances.transpose(1, 0)
         
         word_hidden_state = []
+        sentences = sentences.transpose(1, 0)
         for i in range(batch_size):
             gcn = ONLSTMGraph(distances[i], self.gcn_input_size, self.gcn_hidden_size,
                               self.gcn_output_size)
-            word_hidden_state.append(gcn(torch.zeros([length, self.gcn_input_size])))
+            word_hidden_state.append(gcn(sentences[i]))
             # word_hidden_state.append(gcn(words_embedding))
         return raw_output[-1], word_hidden_state
     def init_hidden(self, bsz):
