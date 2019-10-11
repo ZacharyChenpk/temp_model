0d0da9d6d4f1e8d210bd8af8df9d90755dd4bf3a
tsy19025 <45425656+tsy19025@users.noreply.github.com>
2019-10-11 15:27:26 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,33 +1,108 @@
+import os
 import torch
+import sys
 import numpy as np
 
-### Wraps hidden states in new Tensors to detach them from their history
-def repackage_hidden(h):
-    if isinstance(h, torch.Tensor):
-        return h.detach()
-    else:
-        return tuple(repackage_hidden(v) for v in h)
+from collections import Counter
 
-### Transfer the data into batches
-def batchify(data, bsz, args):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = data.size // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data[0:(nbatch * bsz)]
-    print(data)
-    print(type(data))
-    # Evenly divide the data across the bsz batches.
-    data = data.reshape((bsz, -1)).T ###.contiguous()
+import nltk
+import torch
+### Add the data path to syspath
+sys.path.append('../training')
+from nltk.corpus import treebank
+
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = []
+        self.counter = Counter()
+        self.total = 0
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        token_id = self.word2idx[word]
+        self.counter[token_id] += 1
+        self.total += 1
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
+
+class Corpus(object):
+    """
+        read in dataset and tokenize the corpus
+    """
+    def __init__(self, path):
+        ### Input language dictionary
+        self.dictionary = Dictionary()
+        ### Output language dictionary
+        self.dictionary_out = Dictionary()
+        self.train = self.tokenize(path, path)
+        self.valid = self.tokenize(path, path)
+        self.test = self.tokenize(path, path)
+        '''
+        self.train = self.tokenize(os.path.join(path, 'train_x.txt'), os.path.join(path, 'train_y.txt'))
+        self.valid = self.tokenize(os.path.join(path, 'valid_x.txt'), os.path.join(path, 'valid_y.txt'))
+        self.test = self.tokenize(os.path.join(path, 'test_x.txt'), os.path.join(path, 'test_y.txt'))
+        '''
+        print(type(self.train))
     '''
-    data = torch.Tensor(data)
-    if args.cuda:
-        data = data.cuda()
+    def pairtoken(self, the_tuple, the_tree):
+        x, y = the_tuple
+        xwords = x.split()
+        ywords = y.split()
+        return {'X': list(map(self.dictionary.word2idx.__getitem__, xwords)), 'Y': list(map(self.dictionary_out.word2idx.__getitem__, ywords)), 'Y_tree': the_tree}
     '''
-    return data
+    def sen_tokenize(self, sen, is_out = False):
+        words = sen.split()
+        if not is_out:
+            return [self.dictionary.word2idx[i] for i in words]
+        else:
+            return [self.dictionary_out.word2idx[i] for i in words]
 
+        ### Tokenizes two text files and add to the dictionary
+        ### We stored source sentences in PATH and target sentences in TARGET_PATH
+    def tokenize(self, path, target_path):
+        assert os.path.exists(path)
+        assert os.path.exists(target_path)
+        ### Add words to the dictionary
+        self.dictionary.add_word('<start>')
+        # self.dictionary.add_word('<end>')
+        self.dictionary_out.add_word('<start>')
+        # self.dictionary_out.add_word('<end>')
+        with open(path, 'r') as f:
+            tokens = 0
+            tot = 0
+            for line in f:
+                words = line.split()
+                tokens += len(words)
+                for word in words:
+                    self.dictionary.add_word(word)
+                tot = tot + 1
+                if tot > 10:
+                    break
+        with open(target_path, 'r') as f:
+            tar_tokens = 0
+            tot = 0
+            for line in f:
+                words = line.split()
+                tar_tokens += len(words)
+                for word in words:
+                    self.dictionary_out.add_word(word)
+                if tot > 10:
+                    break
 
-def get_batch(source, i, args, seq_len=None, evaluation=False):
-    seq_len = min(seq_len if seq_len else args.bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
+        ### Tokenize file content
+        '''
+        zipped = list(zip(open(path, 'r'), open(target_path, 'r')))
+        #print(zipped)
+
+        return np.asarray(list(map(self.pairtoken, zipped, torch.load(tree_path))), dtype = tuple)
+        '''
+
+        ret1, ret2 = list(map(self.sen_tokenize, open(path, 'r'))), list(map(lambda x:self.sen_tokenize(x, True), open(target_path, 'r')))
+        assert(len(ret1)==len(ret2))
+
+        return np.asarray(ret1), np.asarray(ret2), list(zip(open(path, 'r')))
